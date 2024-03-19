4e7caeecc11891ab2f65855d508484a52d5a4ef1
heygwangjin <heygwangjin@gmail.com>
2024-03-19 23:37:57 +0900

    feat: Add Dialog Demo

---
+++ src/components/DialogDemo/DialogDemo.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+function DialogDemo() {
+  return (
+    <div>
+      <h1>Dialog Demo</h1>
+      <p>Click the button to open the dialog.</p>
+      <button>Open Dialog</button>
+    </div>
+  )
+}
+
+export default DialogDemo;
---
+++ src/components/DialogDemo/index.ts
@@ -0,0 +1,2 @@
+export * from "./DialogDemo";
+export { default } from "./DialogDemo";
